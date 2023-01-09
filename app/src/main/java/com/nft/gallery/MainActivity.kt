8a378978c6f76eb0ceff3a1a094a760f6ace62d8
Ankur Jain <ankur.jain@solana.com>
2023-01-09 10:43:27 -0800

    Remove write permission since they are overly restrictive in Android 11 and above.

--- app/src/main/java/com/nft/gallery/MainActivity.kt
+++ app/src/main/java/com/nft/gallery/MainActivity.kt
@@ -84,7 +84,6 @@ class MainActivity : ComponentActivity() {
         } else {
             listOf(
                 Manifest.permission.READ_EXTERNAL_STORAGE,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE,
             )
         }
 
@@ -136,7 +135,6 @@ class MainActivity : ComponentActivity() {
         } else {
             listOf(
                 Manifest.permission.READ_EXTERNAL_STORAGE,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE,
             )
         }
 
