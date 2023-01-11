7b33c4f005705c7fd50f59b824bb19c57b4847b9
Olivier Goutay <olivier.goutay@gmail.com>
2023-01-11 23:35:30 +0100

    MyMints: fetching NFTs (#16)
    * Import metaplex SDKs
    
    * Adding jitpack as a dependency repository
    
    * Importing guava directly (issues with gradle)
    
    * Getting back all the NFTs that have a collection
    
    * Wrapping up the Repository and ViewModel
    
    * UI fixes and reset the list of NFTs every time
    
    * Adding publicKey as a constructor param to NFTMintyRepository
    
    * Adding a MyMintsMapper

--- app/src/main/java/com/nft/gallery/composables/MyMints.kt
+++ app/src/main/java/com/nft/gallery/composables/MyMints.kt
@@ -1,8 +1,13 @@
 package com.nft.gallery.composables
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -10,11 +15,16 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.nft.gallery.viewmodel.MyMintsViewModel
+import com.solana.core.PublicKey
 
 @Composable
 fun MyMintPage(
@@ -25,27 +35,52 @@ fun MyMintPage(
     LaunchedEffect(
         key1 = Unit,
         block = {
-            myMintsViewModel.loadMyMints()
+            myMintsViewModel.loadMyMints(
+                PublicKey("5nmoLTjaCYxDY2iZEAHEnbkTyPRrqtF6mrGwXxuJGr4C") // TODO real public key from MWA
+            )
         }
     )
 
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
-            .padding(16.dp)
-            .fillMaxWidth()
+            .padding(top = 16.dp)
+            .padding(horizontal = 16.dp)
     ) {
         Text(
             text = AnnotatedString(
-                "My ",
+                "My",
                 spanStyle = SpanStyle(MaterialTheme.colorScheme.onSurfaceVariant)
             ).plus(
                 AnnotatedString(
-                    "mints",
+                    " mints",
                     spanStyle = SpanStyle(MaterialTheme.colorScheme.onSurface)
                 )
             ),
             style = MaterialTheme.typography.headlineMedium,
+            modifier = Modifier
+                .padding(bottom = 30.dp)
+                .align(Alignment.Start)
         )
+        LazyVerticalGrid(
+            modifier = Modifier.padding(top = 16.dp),
+            columns = GridCells.Adaptive(minSize = 76.dp),
+            verticalArrangement = Arrangement.spacedBy(10.dp),
+            horizontalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
+            itemsIndexed(items = uiState) { _, path ->
+                AsyncImage(
+                    modifier = Modifier
+                        .height(76.dp)
+                        .width(76.dp),
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(path.mediaUrl)
+                        .crossfade(true)
+                        .build(),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                )
+            }
+        }
     }
 }
--- app/src/main/java/com/nft/gallery/viewmodel/MyMintsViewModel.kt
+++ app/src/main/java/com/nft/gallery/viewmodel/MyMintsViewModel.kt
@@ -1,11 +1,16 @@
 package com.nft.gallery.viewmodel
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
+import com.nft.gallery.repository.NFTMintyRepository
+import com.nft.gallery.viewmodel.mapper.MyMintsMapper
+import com.solana.core.PublicKey
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.getAndUpdate
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -16,16 +21,38 @@ data class MyMint(
 
 @HiltViewModel
 class MyMintsViewModel @Inject constructor(
-    application: Application
-): AndroidViewModel(application) {
+    application: Application,
+    private val myMintsMapper: MyMintsMapper,
+) : AndroidViewModel(application) {
+
+    companion object {
+        private val TAG = "MyMintsViewModel"
+    }
 
     private var _viewState: MutableStateFlow<List<MyMint>> = MutableStateFlow(listOf())
 
     val viewState = _viewState.asStateFlow()
 
-    fun loadMyMints() {
+    fun loadMyMints(publicKey: PublicKey) {
         viewModelScope.launch {
-            //Get persisted user address, hit metaplex API for NFTs created by them
+            val nftMintyRepository = NFTMintyRepository(publicKey)
+            _viewState.value = listOf()
+            try {
+                val nfts = nftMintyRepository.getAllNftsFromMinty()
+                nfts.forEach { nft ->
+                    val metadata = nftMintyRepository.getNftsMetadata(nft)
+                    _viewState.getAndUpdate {
+                        it.toMutableList().apply {
+                            myMintsMapper.map(nft, metadata)?.let { myMint ->
+                                add(myMint)
+                            }
+                        }
+                    }
+                }
+            } catch (e: Exception) {
+                // TODO?
+                Log.e(TAG, e.toString())
+            }
         }
     }
 }
