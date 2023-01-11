b8bd956f9e11d58f728d38b45a234126041364ea
Andrew Watson <andrew.watson@solanamobile.com>
2023-01-11 13:37:20 -0800

    Get tilte bar, insets setup correctly.

--- app/src/main/java/com/nft/gallery/activity/MainActivity.kt
+++ app/src/main/java/com/nft/gallery/activity/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.foundation.shape.CornerSize
@@ -16,16 +17,20 @@ import androidx.compose.material.Surface
 import androidx.compose.material.contentColorFor
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.core.view.WindowCompat
 import androidx.navigation.*
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.nft.gallery.composables.Camera
 import com.nft.gallery.composables.Gallery
 import com.nft.gallery.composables.MintDetailsPage
@@ -42,10 +47,21 @@ class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContent {
             val navController = rememberNavController()
             val navBackStackEntry by navController.currentBackStackEntryAsState()
             val currentRoute = navBackStackEntry?.destination?.route
+
+            val systemUiController = rememberSystemUiController()
+            val useDarkIcons = !isSystemInDarkTheme()
+
+            SideEffect {
+                systemUiController.setSystemBarsColor(Color.Transparent, darkIcons = useDarkIcons)
+            }
+
             AppTheme {
                 Scaffold(
                     floatingActionButton = {
@@ -149,75 +165,86 @@ class MainActivity : ComponentActivity() {
             contentColor = contentColorFor(MaterialTheme.colorScheme.surface),
             elevation = 8.dp,
         ) {
-            Row(
-                Modifier
-                    .fillMaxWidth()
-                    .height(80.dp)
-                    .selectableGroup(),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                content = {
-                    val navBackStackEntry by navController.currentBackStackEntryAsState()
-                    val currentRoute = navBackStackEntry?.destination?.route
+            Column {
+                Row(
+                    Modifier
+                        .fillMaxWidth()
+                        .height(80.dp)
+                        .selectableGroup(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    content = {
+                        val navBackStackEntry by navController.currentBackStackEntryAsState()
+                        val currentRoute = navBackStackEntry?.destination?.route
 
-                    items.forEach { item ->
-                        BottomNavigationItem(
-                            icon = {
-                                Column(
-                                    horizontalAlignment = Alignment.CenterHorizontally,
-                                    modifier = Modifier
-                                        .clip(RoundedCornerShape(10.dp))
-                                        .then(
-                                            if (currentRoute == item.route) {
-                                                Modifier.background(
-                                                    color = MaterialTheme.colorScheme.surfaceVariant
-                                                )
-                                            } else {
-                                                Modifier
-                                            }
-                                        )
-                                        .then(
-                                            Modifier
-                                                .padding(horizontal = 12.dp)
-                                        )
-                                ) {
-                                    Icon(
+                        items.forEach { item ->
+                            BottomNavigationItem(
+                                icon = {
+                                    Column(
+                                        horizontalAlignment = Alignment.CenterHorizontally,
                                         modifier = Modifier
-                                            .padding(top = 5.dp, bottom = 3.dp)
-                                            .size(24.dp),
-                                        imageVector = item.icon,
-                                        contentDescription = item.title
-                                    )
-                                    Text(
-                                        text = item.title,
-                                        style = MaterialTheme.typography.bodyMedium,
-                                        modifier = Modifier.padding(bottom = 10.dp)
-                                    )
-                                }
-                            },
-                            selectedContentColor = MaterialTheme.colorScheme.onSurface,
-                            unselectedContentColor = MaterialTheme.colorScheme.onSurfaceVariant,
-                            selected = currentRoute == item.route,
-                            onClick = {
-                                navController.navigate(item.route) {
-                                    // Pop up to the start destination of the graph to
-                                    // avoid building up a large stack of destinations
-                                    // on the back stack as users select items
-                                    navController.graph.startDestinationRoute?.let { route ->
-                                        popUpTo(route) {
-                                            saveState = true
-                                        }
+                                            .clip(RoundedCornerShape(10.dp))
+                                            .then(
+                                                if (currentRoute == item.route) {
+                                                    Modifier.background(
+                                                        color = MaterialTheme.colorScheme.surfaceVariant
+                                                    )
+                                                } else {
+                                                    Modifier
+                                                }
+                                            )
+                                            .then(
+                                                Modifier
+                                                    .padding(horizontal = 12.dp)
+                                            )
+                                    ) {
+                                        Icon(
+                                            modifier = Modifier
+                                                .padding(top = 5.dp, bottom = 3.dp)
+                                                .size(24.dp),
+                                            imageVector = item.icon,
+                                            contentDescription = item.title
+                                        )
+                                        Text(
+                                            text = item.title,
+                                            style = MaterialTheme.typography.bodyMedium,
+                                            modifier = Modifier.padding(bottom = 10.dp)
+                                        )
                                     }
-                                    // Avoid multiple copies of the same destination when
-                                    // re-selecting the same item
-                                    launchSingleTop = true
-                                    // Restore state when re-selecting a previously selected item
-                                    restoreState = true
+                                },
+                                selectedContentColor = MaterialTheme.colorScheme.onSurface,
+                                unselectedContentColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                                selected = currentRoute == item.route,
+                                onClick = {
+                                    navController.navigate(item.route) {
+                                        // Pop up to the start destination of the graph to
+                                        // avoid building up a large stack of destinations
+                                        // on the back stack as users select items
+                                        navController.graph.startDestinationRoute?.let { route ->
+                                            popUpTo(route) {
+                                                saveState = true
+                                            }
+                                        }
+                                        // Avoid multiple copies of the same destination when
+                                        // re-selecting the same item
+                                        launchSingleTop = true
+                                        // Restore state when re-selecting a previously selected item
+                                        restoreState = true
+                                    }
                                 }
-                            }
-                        )
+                            )
+                        }
                     }
-                }
-            )
+                )
+
+                Spacer(
+                    modifier = Modifier
+                        .background(
+                            color = MaterialTheme.colorScheme.background
+                        )
+                        .navigationBarsPadding()
+                        .fillMaxWidth()
+                )
+            }
         }
     }
 }
