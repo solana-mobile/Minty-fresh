abf60f400ba26365248e76bba6ec77185ce480a9
oliveeyay <olivier.goutay@gmail.com>
2023-01-11 22:34:33 +0100

    Wrapping up the Repository and ViewModel

--- app/src/main/java/com/nft/gallery/activity/MyMints.kt
+++ app/src/main/java/com/nft/gallery/activity/MyMints.kt
@@ -1,8 +1,12 @@
 package com.nft.gallery.activity
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -10,12 +14,22 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.nft.gallery.viewmodel.MyMintsViewModel
 
+@OptIn(ExperimentalGlideComposeApi::class)
 @Composable
 fun MyMintPage(
     myMintsViewModel: MyMintsViewModel = hiltViewModel()
@@ -32,20 +46,41 @@ fun MyMintPage(
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
-            .padding(16.dp)
-            .fillMaxWidth()
+            .padding(top = 16.dp)
+            .padding(horizontal = 16.dp)
     ) {
         Text(
             text = AnnotatedString(
-                "My ",
+                "My",
                 spanStyle = SpanStyle(MaterialTheme.colorScheme.onSurfaceVariant)
             ).plus(
                 AnnotatedString(
-                    "mints",
+                    " mints",
                     spanStyle = SpanStyle(MaterialTheme.colorScheme.onSurface)
                 )
             ),
             style = MaterialTheme.typography.headlineMedium,
+            modifier = Modifier.padding(bottom = 30.dp)
         )
+        LazyVerticalGrid(
+            modifier = Modifier.padding(top = 16.dp),
+            columns = GridCells.Adaptive(minSize = 76.dp),
+            verticalArrangement = Arrangement.spacedBy(10.dp),
+            horizontalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
+            itemsIndexed(items = uiState) { _, path ->
+                AsyncImage(
+                    modifier = Modifier
+                        .height(76.dp)
+                        .width(76.dp),
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(path.mediaUrl)
+                        .crossfade(true)
+                        .build(),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                )
+            }
+        }
     }
 }
--- app/src/main/java/com/nft/gallery/viewmodel/MyMintsViewModel.kt
+++ app/src/main/java/com/nft/gallery/viewmodel/MyMintsViewModel.kt
@@ -1,23 +1,15 @@
 package com.nft.gallery.viewmodel
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
-import com.metaplex.lib.drivers.indenty.IdentityDriver
-import com.metaplex.lib.drivers.rpc.JdkRpcDriver
-import com.metaplex.lib.drivers.solana.Commitment
-import com.metaplex.lib.drivers.solana.SolanaConnectionDriver
-import com.metaplex.lib.drivers.solana.TransactionOptions
-import com.metaplex.lib.modules.nfts.NftClient
-import com.solana.core.PublicKey
-import com.solana.core.Transaction
+import com.nft.gallery.repository.NFTMintyRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.flow.getAndUpdate
 import kotlinx.coroutines.launch
-import java.net.URL
 import javax.inject.Inject
 
 data class MyMint(
@@ -27,54 +19,36 @@ data class MyMint(
 
 @HiltViewModel
 class MyMintsViewModel @Inject constructor(
-    application: Application
+    application: Application,
+    private val nftMintyRepository: NFTMintyRepository,
 ) : AndroidViewModel(application) {
 
-    private val pubKey = PublicKey("5nmoLTjaCYxDY2iZEAHEnbkTyPRrqtF6mrGwXxuJGr4C")
-    private val connection = SolanaConnectionDriver(
-        JdkRpcDriver(URL("https://solana-mainnet.g.alchemy.com/v2/wNKQI1tTf6CBkHRo7fQGlyQxCQVy1pxj")),
-        TransactionOptions(Commitment.CONFIRMED, skipPreflight = true)
-    )
-    private val identityDriver = object : IdentityDriver {
-        // fill in, only the publicKey attribute is needed for lookups
-        override val publicKey = pubKey
-
-        override fun sendTransaction(
-            transaction: Transaction,
-            recentBlockHash: String?,
-            onComplete: (Result<String>) -> Unit
-        ) {
-        }
-
-        override fun signAllTransactions(
-            transactions: List<Transaction>,
-            onComplete: (Result<List<Transaction?>>) -> Unit
-        ) {
-        }
-
-        override fun signTransaction(
-            transaction: Transaction,
-            onComplete: (Result<Transaction>) -> Unit
-        ) {
-        }
+    companion object {
+        private val TAG = "MyMintsViewModel"
     }
-    private val nftClient = NftClient(connection, identityDriver)
 
     private var _viewState: MutableStateFlow<List<MyMint>> = MutableStateFlow(listOf())
 
     val viewState = _viewState.asStateFlow()
 
     fun loadMyMints() {
-        viewModelScope.launch(Dispatchers.IO) {
-            _viewState.update {
-                try {
-                    nftClient.findAllByOwner(pubKey).getOrThrow()
-                        .filterNotNull()
-                        .filter { it.collection != null }
-                        .map { MyMint(it.mint.toString(), it.uri) }
-                } catch (e: Exception) {
-                    listOf()
+        viewModelScope.launch {
+            try {
+                val nfts = nftMintyRepository.getAllNftsFromMinty()
+                nfts.forEach { nft ->
+                    nftMintyRepository.getNftsMetadata(nft).image?.let { imageUrl ->
+                        _viewState.getAndUpdate {
+                            it.toMutableList().apply {
+                                add(
+                                    MyMint(nft.mint.toString(), imageUrl)
+                                )
+                            }
+                        }
+                    }
                 }
+            } catch (e: Exception) {
+                // TODO?
+                Log.e(TAG, e.toString())
             }
         }
     }
