fc653d773b02418bf1f25973d77b5a4e14ad9137
Andrew Watson <andrew.watson@solanamobile.com>
2023-01-13 13:28:03 -0800

    Make PersistenceUseCase full Flow-based. NOTE: Build is broken as of this commit

--- app/src/main/java/com/nft/gallery/usecase/PersistenceUseCase.kt
+++ app/src/main/java/com/nft/gallery/usecase/PersistenceUseCase.kt
@@ -1,8 +1,8 @@
 package com.nft.gallery.usecase
 
-import android.content.SharedPreferences
 import com.nft.gallery.repository.PrefsDataStoreRepository
 import com.portto.solana.web3.PublicKey
+import kotlinx.coroutines.flow.combine
 import javax.inject.Inject
 
 sealed class UserWalletDetails
@@ -17,40 +17,32 @@ data class Connected(
 
 class PersistenceUseCase @Inject constructor(
     private val dataStoreRepository: PrefsDataStoreRepository,
-    private val sharedPreferences: SharedPreferences
 ) {
 
     private var connection: UserWalletDetails = NotConnected
 
-    fun getWalletConnection(): UserWalletDetails {
-        return when(connection) {
-            is Connected -> connection
-            is NotConnected -> {
-                val key = sharedPreferences.getString(PUBKEY_KEY, "")
-                val accountLabel = sharedPreferences.getString(ACCOUNT_LABEL, "") ?: ""
-                val token = sharedPreferences.getString(AUTH_TOKEN_KEY, "")
-
-                val newConn = if (key.isNullOrEmpty() || token.isNullOrEmpty()) {
-                    NotConnected
-                } else {
-                    Connected(PublicKey(key), accountLabel, token)
-                }
-
-                return newConn
-            }
+    val walletDetails = combine(
+        dataStoreRepository.publicKeyFlow,
+        dataStoreRepository.accountLabelFlow,
+        dataStoreRepository.authTokenFlow)
+    { pubKey, label, authToken ->
+        if (pubKey.isEmpty() && label.isEmpty() && authToken.isEmpty()) {
+            NotConnected
+        } else {
+            Connected(
+                publicKey = PublicKey(pubKey),
+                accountLabel = label,
+                authToken = authToken
+            )
         }
     }
 
     suspend fun persistConnection(pubKey: PublicKey, accountLabel: String, token: String) {
         dataStoreRepository.updateWalletDetails(pubKey.toBase58(), accountLabel, token)
-
-        connection = Connected(pubKey, accountLabel, token)
     }
 
     suspend fun clearConnection() {
         dataStoreRepository.clearWalletDetails()
-
-        connection = NotConnected
     }
 
     companion object {
