73b6e9cbe150e958db5a9baba56412ab2175fd63
oliveeyay <olivier.goutay@gmail.com>
2023-01-16 15:03:36 +0100

    Improving the version name script and fixing build.gradle

--- scripts/publish/updateVersionName.sh
+++ scripts/publish/updateVersionName.sh
@@ -17,5 +17,5 @@ previousVersionName=$(echo $string | cut -d= -f2 | tr -d \''"\')
 
 # Replacing the previous version
 echo "Will replace previous version $previousVersionName by new version $newVersionName in $gradleConfigurationFile"
-sed -i'.bak' "s/\($previousVersionName\)\(.*\)/$newVersionName\'/" $gradleConfigurationFile
+sed -i'.bak' "s/\($versionNameField \)\(.*\)/$versionNameField \'$newVersionName\'/" $gradleConfigurationFile
 rm ${gradleConfigurationFile}.bak # remove backup file
