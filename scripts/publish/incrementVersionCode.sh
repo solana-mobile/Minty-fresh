5eaba5366e2709ee7d80b3e978784f6cd2042255
oliveeyay <olivier.goutay@gmail.com>
2023-01-16 14:59:50 +0100

    Adding bash scripts to increment versionCode and name after each release

---
+++ scripts/publish/incrementVersionCode.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+gradleConfigurationFile=app/build.gradle
+versionCodeField="versionCode"
+
+# Getting the version code
+grep -n $versionCodeField $gradleConfigurationFile | cut -d: -f2
+string=$(grep -n $versionCodeField $gradleConfigurationFile | cut -d: -f2)
+
+# Incrementing the version
+versionCode=$(echo $string | cut -d= -f2 | sed "s/$versionCodeField//" | tr -d ' ')
+newVersionCode=$(( versionCode + 1 ))
+
+# Replacing the previous version
+echo "Will replace previous version $versionCode by new version $newVersionCode in $gradleConfigurationFile"
+sed -i'.bak' "s/\($versionCodeField \)\(.*\)/$versionCodeField $newVersionCode/" $gradleConfigurationFile
+rm ${gradleConfigurationFile}.bak # remove backup file
---
+++ scripts/publish/updateVersionName.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+newVersionName=$1
+if [ -z $newVersionName ]
+  then
+    echo "No newVersionName argument supplied, it is required"
+    exit 1
+fi
+
+gradleConfigurationFile=app/build.gradle
+versionNameField="versionName"
+
+# Getting the version name
+grep -n $versionNameField $gradleConfigurationFile | cut -d: -f2
+string=$(grep $versionNameField $gradleConfigurationFile | awk -F[\'\'] '{print $2}' | tr -d \''"\')
+previousVersionName=$(echo $string | cut -d= -f2 | tr -d \''"\')
+
+# Replacing the previous version
+echo "Will replace previous version $previousVersionName by new version $newVersionName in $gradleConfigurationFile"
+sed -i'.bak' "s/\($previousVersionName\)\(.*\)/$newVersionName\'/" $gradleConfigurationFile
+rm ${gradleConfigurationFile}.bak # remove backup file
