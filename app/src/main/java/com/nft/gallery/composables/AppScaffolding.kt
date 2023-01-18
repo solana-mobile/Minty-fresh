eba70f3da2de9a7edbcb6ecfd043fc28fccf33ec
Andrew Watson <andrew.watson@solanamobile.com>
2023-01-18 12:43:17 -0800

    Get a nice bottom sheet layou tin place

--- app/src/main/java/com/nft/gallery/composables/AppScaffolding.kt
+++ app/src/main/java/com/nft/gallery/composables/AppScaffolding.kt
@@ -1,5 +1,6 @@
 package com.nft.gallery.composables
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.selection.selectableGroup
@@ -21,10 +22,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
+import com.nft.gallery.R
 import com.nft.gallery.theme.NavigationItem
 import com.nft.gallery.viewmodel.WalletConnectionViewModel
 import com.solana.mobilewalletadapter.clientlib.ActivityResultSender
@@ -42,9 +46,7 @@ fun ScaffoldScreen(
     content: @Composable () -> Unit
 ) {
     val viewState = walletConnectionViewModel.viewState.collectAsState().value
-    val drawerState = rememberBottomDrawerState(
-        initialValue = BottomDrawerValue.Open
-    )
+    val drawerState = rememberBottomDrawerState(initialValue = BottomDrawerValue.Expanded)
 
     Scaffold(
         floatingActionButton = {
@@ -134,17 +136,68 @@ fun ScaffoldScreen(
                 drawerContent = {
                     Column(
                         modifier = Modifier
+                            .padding(
+                                bottom = 80.dp
+                            )
+                            .navigationBarsPadding()
                             .fillMaxWidth()
-                            .height(450.dp)
+                            .wrapContentHeight()
                             .background(
                                 color = MaterialTheme.colorScheme.background,
                                 shape = RoundedCornerShape(
                                     topStart = 24.dp,
                                     topEnd = 24.dp
                                 )
-                            )
+                            ),
+                        horizontalAlignment = Alignment.CenterHorizontally
                     ) {
-                        Text(text = "hi")
+                        Image(
+                            modifier = Modifier
+                                .padding(
+                                    top = 42.dp
+                                )
+                                .width(293.dp)
+                                .height(122.dp),
+                            painter = painterResource(
+                                id = R.drawable.mint_confirm
+                            ),
+                            contentDescription = "Sparkle image",
+                            contentScale = ContentScale.Inside
+                        )
+                        Text(
+                            modifier = Modifier
+                                .padding(
+                                    top = 19.dp
+                                ),
+                            text = "You minted an NFT!",
+                            style = MaterialTheme.typography.titleLarge,
+                            color = MaterialTheme.colorScheme.onBackground
+                        )
+                        Text(
+                            modifier = Modifier
+                                .padding(
+                                    top = 10.dp
+                                ),
+                            text = "It's been added to your connected wallet",
+                            style = MaterialTheme.typography.bodyMedium,
+                            color = MaterialTheme.colorScheme.onSurface
+                        )
+                        Button(
+                            modifier = Modifier
+                                .padding(
+                                    top = 70.dp,
+                                    bottom = 44.dp
+                                ),
+                            shape = RoundedCornerShape(corner = CornerSize(16.dp)),
+                            colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.onBackground),
+                            onClick = {
+
+                            }
+                        ) {
+                            Text(
+                                text = "Done"
+                            )
+                        }
                     }
                 },
                 drawerState = drawerState,
