3767a7c982e71dc94a8dcfd570f9df19f81e230d
Ankur Jain <ankur.jain@solana.com>
2023-01-20 08:57:04 -0800

    Better path name for images directory

--- app/src/main/java/com/nft/gallery/composables/Camera.kt
+++ app/src/main/java/com/nft/gallery/composables/Camera.kt
@@ -157,7 +157,7 @@ private fun takePhoto(
         put(MediaStore.MediaColumns.DISPLAY_NAME, name)
         put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
         if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
-            put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image")
+            put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/MintyFresh")
         }
     }
 
