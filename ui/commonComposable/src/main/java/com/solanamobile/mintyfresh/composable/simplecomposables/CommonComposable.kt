5b7a1518a1296200229092a05edd1c9f7a375c50
Ankur Jain <ankur.jain@solana.com>
2023-02-03 16:21:31 -0800

    Fix camera permission theme colors (#185)
    * Fix camera theme
    
    * Fix text alignment.

--- ui/commonComposable/src/main/java/com/solanamobile/mintyfresh/composable/simplecomposables/CommonComposable.kt
+++ ui/commonComposable/src/main/java/com/solanamobile/mintyfresh/composable/simplecomposables/CommonComposable.kt
@@ -16,6 +16,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.MultiplePermissionsState
@@ -76,7 +77,11 @@ fun EmptyView(permissionState: MultiplePermissionsState) {
             stringResource(R.string.permission_important, revokedPermissions.joinToString(separator = ", "))
         }
 
-        Text(textToShow, modifier = Modifier.padding(vertical = 16.dp))
+        Text(
+            textToShow,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(vertical = 16.dp)
+        )
 
         Button(
             shape = RoundedCornerShape(corner = CornerSize(16.dp)),
