44ff8023db6304121d790d335d3a2712c080d221
Ankur Jain <ankur.jain@solana.com>
2023-02-08 12:03:51 -0800

    remove unused methods

--- app/src/main/java/com/solanamobile/mintyfresh/navigation/CreationGraph.kt
+++ app/src/main/java/com/solanamobile/mintyfresh/navigation/CreationGraph.kt
@@ -1,15 +1,9 @@
 package com.solanamobile.mintyfresh.navigation
 
-import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
-import androidx.navigation.NavOptions
 import androidx.navigation.navigation
 
 const val creatingGraphRoutePattern = "createGraph"
-
-fun NavController.navigateToCreatingGraph(navOptions: NavOptions? = null) {
-    this.navigate(creatingGraphRoutePattern, navOptions)
-}
 
 fun NavGraphBuilder.creatingGraph(
     startDestination: String,
--- app/src/main/java/com/solanamobile/mintyfresh/navigation/ViewingGraph.kt
+++ app/src/main/java/com/solanamobile/mintyfresh/navigation/ViewingGraph.kt
@@ -1,15 +1,9 @@
 package com.solanamobile.mintyfresh.navigation
 
-import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
-import androidx.navigation.NavOptions
 import androidx.navigation.navigation
 
 const val viewingGraphRoutePattern = "viewingGraph"
-
-fun NavController.navigateToViewingGraph(navOptions: NavOptions? = null) {
-    this.navigate(viewingGraphRoutePattern, navOptions)
-}
 
 fun NavGraphBuilder.viewingGraph(
     startDestination: String,
