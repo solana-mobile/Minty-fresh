6bbc9e01f6cda8da848b64b87b77239d9fd66655
Ankur Jain <ankur.jain@solana.com>
2023-02-08 22:06:45 -0800

    Add some more docs.

--- libs/persistence/src/main/java/com/solanamobile/mintyfresh/persistence/diskcache/MyMintsCacheRepository.kt
+++ libs/persistence/src/main/java/com/solanamobile/mintyfresh/persistence/diskcache/MyMintsCacheRepository.kt
@@ -3,28 +3,50 @@ package com.solanamobile.mintyfresh.persistence.diskcache
 import javax.inject.Inject
 import javax.inject.Singleton
 
+/**
+ * Disk storage backed MyMints cache implementation.
+ * Reads are exclusively from local storage to support offline access.
+ */
 @Singleton
 class MyMintsCacheRepository @Inject constructor(
     private val myMintsDatabaseProvider: MyMintsDatabaseProvider
 ) {
 
+    /**
+     * Inserts [myMints] into the database .
+     */
     suspend fun insertAll(myMints: List<MyMint>) =
         myMintsDatabaseProvider.roomDb.myMintsDao().insertAll(myMints)
 
-    suspend fun deleteStaleData(currentMintList: List<MyMint>, pubKey: String) {
+    /**
+     * Deletes stale data for [pubKey] and [clusterName] which is not in [currentMintList].
+     */
+    suspend fun deleteStaleData(
+        currentMintList: List<MyMint>,
+        clusterName: String,
+        pubKey: String
+    ) {
         val newIds = currentMintList.map { it.id }
-        myMintsDatabaseProvider.roomDb.myMintsDao().deleteStaleData(pubKey, newIds)
+        myMintsDatabaseProvider.roomDb.myMintsDao()
+            .deleteStaleData(pubKey, clusterName = clusterName, latestNftIds = newIds)
     }
 
-    fun get(pubKey: String, rpcClusterName: String) =
+    /**
+     * Stream of [MyMint] for given [pubKey] and [clusterName]
+     */
+    fun get(pubKey: String, clusterName: String) =
         myMintsDatabaseProvider.roomDb.myMintsDao().get(
             pubKey = pubKey,
-            clusterName = rpcClusterName
+            clusterName = clusterName
         )
 
-    suspend fun get(id: String, pubKey: String, rpcClusterName: String) = myMintsDatabaseProvider.roomDb.myMintsDao().get(
-        id = id,
-        pubKey = pubKey,
-        clusterName = rpcClusterName
-    )
+    /**
+     * Returns [MyMint] for given (NFT id [id]), (PublicKey [pubKey]) and [clusterName]
+     */
+    suspend fun get(id: String, pubKey: String, clusterName: String) =
+        myMintsDatabaseProvider.roomDb.myMintsDao().get(
+            id = id,
+            pubKey = pubKey,
+            clusterName = clusterName
+        )
 }
