9f079938d8787ed013883f3aa4e3f9c8a0156b22
Andrew Watson <andrew.watson@solanamobile.com>
2023-03-08 15:33:00 -0800

    Utilize launched effect so navigation doesn't get spammed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,7 +87,7 @@ dependencies {
     implementation("androidx.compose.material:material:1.4.0-beta01")
     implementation("androidx.compose.material:material-icons-core:1.4.0-beta01")
     implementation("androidx.compose.material:material-icons-extended:1.4.0-beta01")
-    implementation("androidx.activity:activity-compose:1.7.0-alpha03")
+    implementation("androidx.activity:activity-compose:1.6.1")
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
     implementation("androidx.navigation:navigation-compose:2.5.3")
     implementation("androidx.compose.ui:ui-tooling-preview:1.3.3")
