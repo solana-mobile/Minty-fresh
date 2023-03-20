8c3a540935925fa73353ebaf375ea7650aa2e430
Ankur Jain <ankur.jain@solana.com>
2023-03-20 12:45:01 -0700

    Disable lint

--- ui/commonComposable/build.gradle.kts
+++ ui/commonComposable/build.gradle.kts
@@ -34,6 +34,11 @@ android {
             )
         }
     }
+
+    lint {
+        disable.add("MissingTranslation")
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
